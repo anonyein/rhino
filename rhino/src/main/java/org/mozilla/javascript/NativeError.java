0c422d8ebdd07ce9cbce7f78f61b76a28e0f2a74
anonyein <48914448+anonyein@users.noreply.github.com>
2025-08-27 11:03:55 +0800

    Update NativeError.java

--- rhino/src/main/java/org/mozilla/javascript/NativeError.java
+++ rhino/src/main/java/org/mozilla/javascript/NativeError.java
@@ -259,7 +259,7 @@ final class NativeError extends IdScriptableObject {
         // Determine whether to format the stack trace ourselves, or call the user's code to do it
         Object value;
         if (prepare == null) {
-            value = RhinoException.formatStackTrace(stackTrace, stackProvider.toString());
+            value = RhinoException.formatStackTrace(stackTrace, stackProvider.getMessage());
         } else {
             value = callPrepareStack(prepare, stackTrace);
         }
